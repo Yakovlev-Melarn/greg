ad13afc67aed77cc630760db2f821ad3415b2d3f
Admin <melarn4u@gmail.com>
2025-08-05 19:22:34 +0300

    dashboard menu

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -12,13 +12,10 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
-
         root.render(<App {...props} />);
     },
     progress: {
         color: '#4B5563',
     },
 });
-
-// This will set light / dark mode on load...
 initializeTheme();
