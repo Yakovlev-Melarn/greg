1e9af9ed007af0fd95f437d0b542f4e28c423ecd
Admin <melarn4u@gmail.com>
2025-08-24 13:43:03 +0300

    Seller Session

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,9 +8,6 @@ class IndexController
 {
     public function index(Request $request)
     {
-        if (!empty($request->get('seller'))) {
-            session(['seller' => $request->get('seller')]);
-        }
         return view('Index/index');
     }
 }
---
+++ app/Http/Middleware/SessionMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Sellers;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SessionMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->session()->has('seller')) {
+            $seller = Sellers::all()->first();
+            $request->session()->put('seller', $seller->id);
+        }
+        if (!empty($request->get('seller'))) {
+            $request->session()->put('seller', $request->get('seller'));
+        }
+        return $next($request);
+    }
+}
